75de222776c000950964a10a6f4dee0e1c0c3795
Ronit Kumar <roizlive69@gmail.com>
2025-02-26 07:00:37 +0530

    solved the exercise

--- dicts1.py
+++ dicts1.py
@@ -1,10 +1,15 @@
-# dict1 = {'k1':10 , 'k2':20 , 'k3':30 , 'k4':40 }
-# print(dict1.keys())
-# print(dict1.values())
-# print(dict1.items())
-# print(dict1['k3'])
+# # dict1 = {'k1':10 , 'k2':20 , 'k3':30 , 'k4':40 }
+# # print(dict1.keys())
+# # print(dict1.values())
+# # print(dict1.items())
+# # print(dict1['k3'])
 
-dict1 = {'k1': [10 , 20 , 30 ]}
-print(dict1)
+# dict1 = {'k1': [10 , 20 , 30 ]}
+# print(dict1)
 
-print(dict1['k1'][2])
+# print(dict1['k1'][2])
+
+d3 = {'k1' :  [{'nest_key' : ['this is deep' , ['hello']]}]}
+
+d3['k1'][0]['nest_key'][1] = ' Good Bye'
+print(d3)
--- list1.py
+++ list1.py
@@ -1,38 +1,44 @@
-# mylist = [1,2,3,4,5,6]
+# # mylist = [1,2,3,4,5,6]
 
-# print(len(mylist))
+# # print(len(mylist))
 
-# print(mylist)
+# # print(mylist)
 
-# print(mylist[3])
+# # print(mylist[3])
 
-# print(mylist[:3])
+# # print(mylist[:3])
 
-# print(mylist[3:])
+# # print(mylist[3:])
 
-# mylist.append(1)
+# # mylist.append(1)
 
-# print(mylist)
+# # print(mylist)
 
-# mylist.insert(2,'second index ')
+# # mylist.insert(2,'second index ')
 
-# print(mylist)
+# # print(mylist)
 
-# mylist.pop()
-# mylist.pop()
-# mylist.pop()
-# print(mylist)
+# # mylist.pop()
+# # mylist.pop()
+# # mylist.pop()
+# # print(mylist)
 
-# mylist.pop(2)
-# print(mylist)
+# # mylist.pop(2)
+# # print(mylist)
 
-list1 = [ 1 , 2 ,3 ]
-list2 = [ 4 , 5 ,6 ]
-list3 =[7 , 8 , 9 ]
+# list1 = [ 1 , 2 ,3 ]
+# list2 = [ 4 , 5 ,6 ]
+# list3 =[7 , 8 , 9 ]
 
-mega_list = [list1 , list2 , list3 ]
+# mega_list = [list1 , list2 , list3 ]
 
-print(mega_list)
+# print(mega_list)
 
-print(mega_list[0:2])
-print(mega_list[0][2])
+# print(mega_list[0:2])
+# print(mega_list[0][2])
+
+
+mylist = [ 1 , 2 , [ 3 , 4 , 'hello'] ]
+mylist[2][2] = 'Good Bye'
+
+print(mylist)
